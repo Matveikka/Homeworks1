2dcf8153bcdbcb6b18b81741d2d90e672e6cdee3
Matveikka <matwei.sklyarenko@yandex.ru>
2024-07-07 18:19:46 +0300

    Update

--- Homeworks/module2hard.py
+++ Homeworks/module2hard.py
@@ -1,5 +1,25 @@
-set1 = (3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-set2 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+def num_to_password(num: int) -> str:
+    password = ""
+    if num % 2 == 0:
+        iter = (num // 2) - 1
+    else:
+        iter = num // 2
+    for i in range(1, iter + 1):
+        j = num - i
+        password += str(i)
+        password += str(j)
+    return password
+def multiples_to_password(num: int):
+    password = ""
+    num = num
+    for k in range(3, num):
+        if num % k == 0:
+            print(f"Найдено кратное число для {num}: {k}")
+            password += num_to_password(k)
+    return password
 a = int(input())
+result = num_to_password(a)
+result += multiples_to_password(a)
+print(result)
 
 
