e33b15c69c6354986c8de75dc2f2e5fe714ced3c
Matveikka <matwei.sklyarenko@yandex.ru>
2024-07-07 19:07:06 +0300

    !

--- Homeworks/module 3 4.py
+++ Homeworks/module 3 4.py
@@ -3,6 +3,6 @@ def single_root_words(root_word, *other_words):
     for i in other_words:
         if root_word.lower() in i.lower() or i.lower() in root_word.lower():
             same_words.append(i)
-    print(same_words)
+    print (same_words)
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
