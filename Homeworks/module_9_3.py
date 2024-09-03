b4061f8c11314220938f6fca61cac9edd8debda8
Matveikka <matwei.sklyarenko@yandex.ru>
2024-09-03 21:30:32 +0300

    !

--- Homeworks/module_9_3.py
+++ Homeworks/module_9_3.py
@@ -3,4 +3,4 @@ second = ['Строка', 'Урбан', 'Компьютер']
 first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
 second_result = (len(first[i]) <= len(second[j]) for i in range(len(first)) for j in range(len(second)) if i == j)
 print(list(first_result))
-print(list(second_result))
+ print(list(second_result))
