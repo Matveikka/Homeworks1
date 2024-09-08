76b12813e9acb0a3ddf1b741366a9f22fe5634c1
Matveikka <matwei.sklyarenko@yandex.ru>
2024-09-08 20:27:50 +0300

    !

---
+++ Homeworks/module_10_3.py
@@ -0,0 +1,38 @@
+import threading
+import random
+import time
+class Lock:
+    pass
+class Bank:
+    def __init__(self):
+        super().__init__()
+        self.balance = 0
+        self.lock = threading.Lock()
+    def deposit(self):
+        for i in range(100):
+            a = random.randint(50, 500)
+            self.balance += a
+            print(f"Пополнение: {a}. Баланс: {self.balance}")
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            time.sleep(0.001)
+    def take(self):
+        for i in range(100):
+            a = random.randint(50, 500)
+            print(f'Запрос на {a}')
+            if a <= self.balance:
+                self.balance -= a
+                print(f"Снятие: {a}. Баланс: {self.balance}")
+            if a > self.balance:
+                print(f"Запрос отклонён, недостаточно средств")
+                self.lock.acquire()
+bk = Bank()
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+print(f'Итоговый баланс: {bk.balance}')
+
+
