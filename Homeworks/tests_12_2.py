a13130cbae49ee9c453d6dbe4af6ea8ce6cfdcfe
Matveikka <matwei.sklyarenko@yandex.ru>
2024-10-29 19:45:34 +0300

    !

--- Homeworks/tests_12_2.py
+++ Homeworks/tests_12_2.py
@@ -12,23 +12,29 @@ class TournamentTest(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        for i, j in cls.all_results.items():
-            print(f'{i}: {j}')
+        for key in sorted(cls.all_results.keys()):
+            result_dict = cls.all_results[key]
+            formatted_result = {i: j.name for i, j in result_dict.items()}
+            print(formatted_result)
 
-    def start_tournament_1(self):
+    def test_tournament_1(self):
         t1 = R1.Tournament(90, self.runner1, self.runner3)
-        all_results = t1.start()
-        self.assertTrue(all_results[2], self.runner3)
+        result = t1.start()
+        self.assertTrue(result[2], self.runner3)
+        self.all_results['test_tournament_1'] = result
 
-    def start_tournament_2(self):
+    def test_tournament_2(self):
         t2 = R1.Tournament(90, self.runner2, self.runner3)
-        all_results = t2.start()
-        self.assertTrue(all_results[2], self.runner3)
+        result = t2.start()
+        self.assertTrue(result[2], self.runner3)
+        self.all_results['test_tournament_2'] = result
 
-    def start_tournament_3(self):
+    def test_tournament_3(self):
         t3 = R1.Tournament(90, self.runner1, self.runner2, self.runner3)
-        all_results = t3.start()
-        self.assertTrue(all_results[3], self.runner3)
+        result = t3.start()
+        self.assertTrue(result[3], self.runner3)
+        self.all_results['test_tournament_3'] = result
 
 if __name__ == '__main__':
     unittest.main()
+
