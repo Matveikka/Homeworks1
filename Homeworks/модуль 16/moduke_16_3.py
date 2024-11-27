23c9c26ea449f5f7bc0d3675ef144e48b4cdf1a1
Matveikka <matwei.sklyarenko@yandex.ru>
2024-11-27 21:07:49 +0300

    !

---
+++ Homeworks/модуль 16/moduke_16_3.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+users = {'1': 'Имя: Example, возраст: 18'}
+
+
+@app.get('/users')
+async def get_all_users() -> dict:
+    return users
+
+
+@app.post('/user/{username}/{age}')
+async def create_user(username: Annotated[str, Path(min_length=5, max_length=15, description="Enter username")],
+                      age: Annotated[int, Path(ge=18, le=120, description="Enter age")]) -> dict:
+    user_id = str(int(max(users, key=int)) + 1)
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return {'message': f'User {user_id} is registered'}
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: Annotated[str, Path()],
+                      username: Annotated[str, Path(min_length=5, max_length=15, description="Enter username")],
+                      age: Annotated[int, Path(ge=18, le=120, description="Enter age")]):
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} has been updated"
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: Annotated[str, Path()]):
+    del users[user_id]
+    return f"User {user_id} has been deleted"
